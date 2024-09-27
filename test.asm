a088008180699d74f2cbe7e1291d1fe0ecbd7a4e
Kendall Fonseca <kendallfh2004@outlook.com>
2024-09-27 17:59:32 -0600

    Update test.asm

--- test.asm
+++ test.asm
@@ -51,41 +51,62 @@ start:
 
 	; Dibuja bordes
 	MOV CX, 590
-	MOV DX, 50
+	MOV DX, 90
 BORDES_VERTICALES:
 	PINTA_PIXEL 50, DX, 07h  ; Borde izquierdo
-	PINTA_PIXEL 590, DX, 07h ; Borde derecho
+	PINTA_PIXEL 500, DX, 07h ; Borde derecho
 	INC DX
 	CMP DX, 380
 	JBE BORDES_VERTICALES
 
 	; Dibuja bordes horizontales
-	MOV CX, 590
+	MOV CX, 500
 	MOV DX, 50
 BORDES_HORIZONTALES:
-	PINTA_PIXEL CX, 50, 07h  ; Borde superior
+	PINTA_PIXEL CX, 90, 07h  ; Borde superior
 	PINTA_PIXEL CX, 380, 07h ; Borde inferior
 	DEC CX
 	CMP CX, 50
 	JAE BORDES_HORIZONTALES
 
 	; Rellenar el área interna
-	MOV DX, 51     ; Empieza en Y = 51
+	MOV DX, 90     ; Empieza en Y = 51
 RELLENAR_FILAS:
 	MOV CX, 51     ; Empieza en X = 51
 RELLENAR_COLUMNAS:
 	PINTA_PIXEL CX, DX, 07h
 	INC CX
-	CMP CX, 590    ; Hasta la columna 590
+	CMP CX, 500    ; Hasta la columna 590
 	JBE RELLENAR_COLUMNAS
 	INC DX
 	CMP DX, 380    ; Hasta la fila 380
 	JBE RELLENAR_FILAS
 
+	DIBUJAR_RECTANGULO macro x_inicial, y_inicial, ancho, alto, color
+		local FILAS_RECTANGULO, COLUMNAS_RECTANGULO
+	
+		mov di, y_inicial
+	FILAS_RECTANGULO:
+		mov si, x_inicial
+	COLUMNAS_RECTANGULO:
+		PINTA_PIXEL si, di, color
+		inc si
+		cmp si, x_inicial + ancho
+		jb COLUMNAS_RECTANGULO
+		inc di
+		cmp di, y_inicial + alto
+		jb FILAS_RECTANGULO
+	endm
+DIBUJAR_RECTANGULO 3750, 90, 50, 300, 07h 
 	; Dibuja tres cuadrados rojos donde quieras
-	DIBUJAR_CUADRADO 100, 400, 30, 04h ; Primer cuadrado rojo en (100, 400) de tamaño 50x50
-	DIBUJAR_CUADRADO 150, 400, 30, 01h ; Segundo cuadrado rojo en (200, 400) de tamaño 50x50
-	DIBUJAR_CUADRADO 200, 400, 30, 02h ; Tercer cuadrado rojo en (300, 400) de tamaño 50x50
+	DIBUJAR_CUADRADO 3760, 350, 30, 01h ; Primer cuadrado rojo en (100, 400) de tamaño 50x50
+	DIBUJAR_CUADRADO 3760, 300, 30, 02h
+	DIBUJAR_CUADRADO 3760, 250, 30, 04h
+	DIBUJAR_CUADRADO 3760, 200, 30, 06h
+	DIBUJAR_CUADRADO 3760, 150, 30, 09h
+	DIBUJAR_CUADRADO 3760, 100, 30, 13h
+
+	 ; Primer rectángulo con ancho 100 y alto 50
 
 	; Fin del programa
 	MOV AH, 4Ch
